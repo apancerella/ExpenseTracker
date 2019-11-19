fb6b9e332d4aa6a49c47da0b5d3e00e914cd10e2
Anthony P Pancerella <anthony.pancerella@nreca.coop>
2019-11-19 14:00:16 -0500

    removed client build

--- src/server/index.js
+++ src/server/index.js
@@ -14,7 +14,7 @@ const app = express();
 connectDb(AppConfigs.database, AppConfigs.eraseDatabaseOnSync);
 
 // app.use(cors());
-// app.use(compression());
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(async (req, res, next) => {
@@ -24,11 +24,10 @@ app.use(async (req, res, next) => {
 
 app.use('/api', routes);
 
-if(AppConfigs.environment === 'production'){
-  app.use(express.static('../client/build'));
-
-  app.get('/', () => (req, res) => {
-    res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
+if(AppConfigs.environment === 'development'){
+  app.use(express.static(path.join(__dirname, '../../client/build')));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
   });
 }
 
